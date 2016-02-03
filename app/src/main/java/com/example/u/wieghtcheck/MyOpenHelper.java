dc59d2e3c266e8a0c3f1754ca7e10d709a0634d9
ppcrn <ppcrn-22ivyoxy@hotmail.com>
2016-02-03 11:02:47 +0700

    Create MyOpenHelper Successful

--- app/src/main/java/com/example/u/wieghtcheck/MyOpenHelper.java
+++ app/src/main/java/com/example/u/wieghtcheck/MyOpenHelper.java
@@ -1,12 +1,33 @@
 package com.example.u.wieghtcheck;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by u on 3/2/2559.
  */
-public class MyOpenHelper {
+public class MyOpenHelper extends SQLiteOpenHelper{
 
-    public MyOpenHelper() {
+    // Explicit
+    public static final String database_name = "Weight.db"; // ประกาศค่าคงที่ ที่ไม่สามารถแก้ไข้ได้
+    private static final int database_version = 1;
+    private static final String create_table_weight = "create table weightTABLE (" +
+            "_id integer primary key," +
+            "Date text," +
+            "Weight text);";
+
+    public MyOpenHelper(Context context) {
+        super(context, database_name, null, database_version);
     }   // constructor
 
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(create_table_weight);
+    }
 
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }       // Main Class
