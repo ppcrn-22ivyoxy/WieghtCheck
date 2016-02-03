203e9d15eeca38c8d251cff45fa933412c3a84e0
ppcrn <ppcrn-22ivyoxy@hotmail.com>
2016-02-03 10:31:01 +0700

    Create Constructor

---
+++ app/src/main/java/com/example/u/wieghtcheck/MyOpenHelper.java
@@ -0,0 +1,12 @@
+package com.example.u.wieghtcheck;
+
+/**
+ * Created by u on 3/2/2559.
+ */
+public class MyOpenHelper {
+
+    public MyOpenHelper() {
+    }   // constructor
+
+
+}       // Main Class
