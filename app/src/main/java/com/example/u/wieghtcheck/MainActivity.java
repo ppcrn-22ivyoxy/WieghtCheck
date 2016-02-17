fe5cbc41073693aed9ee68449c8d5f0ddb173eff
ppcrn <ppcrn-22ivyoxy@hotmail.com>
2016-02-17 11:08:53 +0700

    Design All Finish

--- app/src/main/java/com/example/u/wieghtcheck/MainActivity.java
+++ app/src/main/java/com/example/u/wieghtcheck/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         objMyManage = new MyManage(this);
 
         //Check Register
-        checkRegister();
+       //checkRegister();
 
     }   // Main Method
 
